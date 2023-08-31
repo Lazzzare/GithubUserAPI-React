0ff6cf4cf56acf40dbfebf9c94cb477a10d1fb9b
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-08-31 04:19:50 +0400

    fix input error problem

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,12 +3,18 @@ import axios from "axios";
 import { SetStateAction, useState, useEffect } from "react";
 import UserInfo from "./UserInfo";
 import UserDataInterface from "./Interface";
+import { useForm } from "react-hook-form";
 
 interface InputProps {
   darkMode: boolean;
 }
 
 const Input = ({ darkMode }: InputProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [value, setValue] = useState("");
   const [userData, setUserData] = useState<UserDataInterface | undefined>(
     undefined
@@ -47,21 +53,24 @@ const Input = ({ darkMode }: InputProps) => {
     <div className={!darkMode ? "dark" : ""}>
       <div className="px-6 md:px-[97px] md:max-w-[930px] m-auto">
         <div
-          className="bg-[#1E2A47] dark:bg-[#FEFEFE] flex flex-row
+          className="relative bg-[#1E2A47] dark:bg-[#FEFEFE] flex flex-row
     justify-between pl-4 py-[7px] pr-[7px] items-center rounded-[15px] mx-auto"
         >
-          <img src={Search} alt="Search" />
-          <input
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            type="text"
-            placeholder="Search GitHub username…"
-            className="w-[70%] bg-[#1E2A47] dark:bg-[#FEFEFE] text-white dark:text-[#4B6A9B] text-xs md:text-lg leading-6 focus:outline-none"
-          />
-
-          <p className="text-[#F74646] font-bold text-[10px] md:text-[15px] md:p-0 mr-1">
-            {githubError}
-          </p>
+          <div className="flex flex-row gap-4 w-full">
+            <img src={Search} alt="Search" />
+            <input
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              type="text"
+              placeholder="Search GitHub username"
+              className="w-[70%] bg-[#1E2A47] dark:bg-[#FEFEFE] text-white dark:text-[#4B6A9B] text-xs md:text-lg leading-6 focus:outline-none"
+            />
+          </div>
+          {githubError ? (
+            <p className="absolute right-32 text-[#F74646] font-bold text-[10px] md:text-[15px] md:p-0 mr-1">
+              {githubError}
+            </p>
+          ) : null}
 
           <button
             className="py-[12.5px] px-4 bg-[#0079FF] rounded-[10px]"
